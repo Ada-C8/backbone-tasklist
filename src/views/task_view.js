b2dd2f327c4766a664b2c6279b0e82882ca38828
dee <dee@adadevelopersacademy.org>
2017-12-11 13:00:07 -0800

    make task view with delete and toggle complete functionality

---
+++ src/views/task_view.js
@@ -0,0 +1,32 @@
+import Backbone from 'backbone';
+import Task from '../models/task';
+
+const TaskView = Backbone.View.extend({
+    initialize(params) {
+      this.template = params.template;
+      this.listenTo(this.model, "change", this.render);
+    },
+    render() {
+      const compiledTemplate = this.template(this.model.toJSON());
+      this.$el.html(compiledTemplate);
+      if (this.model.get('is_complete')) {
+        this.$el.addClass('is-complete');
+      } else {
+        this.$el.removeClass('is-complete');
+      }
+      return this;
+    },
+    events: {
+      'click button.delete': 'deleteTask',
+      'click button.toggle-complete': 'toggleComplete'
+    },
+    deleteTask: function(e) {
+      this.model.destroy();
+      this.remove();
+    },
+    toggleComplete: function(e) {
+      this.model.set('is_complete', !this.model.get('is_complete'));
+    }
+});
+
+export default TaskView;
