6e362d5fa7cf6c5a4c12b0af0cb3db10428df8b1
dee <dee@adadevelopersacademy.org>
2017-12-11 14:12:58 -0800

    move task list rendering and add task logic to task list view

--- src/app.js
+++ src/app.js
@@ -9,68 +9,16 @@ import './css/style.css';
 import Task from './models/task';
 import TaskList from './collections/task_list';
 
-import TaskView from './views/task_view'
+import TaskListView from './views/task_list_view';
 
 const taskList = new TaskList();
 
-const renderList = function(taskList) {
-  const $taskList = $('#todo-items');
-  $taskList.empty();
-
-  taskList.forEach((task) =>{
-    const taskView = new TaskView({
-      model: task,
-      template: _.template($('#task-template').html()),
-      tagName: 'li',
-      className: 'task'
-    });
-
-    $taskList.append(taskView.render().$el);
-  });
-}
-
-// helper method for updating the DOM with the status from a hash
-const updateStatusMessageFrom = (messageHash) => {
-  $('#status-messages').empty();
-  _.each(messageHash, (messageType) => {
-    messageType.forEach((message) => {
-      $('#status-messages').append($(`<li>${message}</li>`));
-    })
-  });
-  $('#status-messages').show();
-}
-
-// helper method for updating the DOM with the status from a string
-const updateStatusMessageWith = (message) => {
-  $('#status-messages').empty();
-  $('#status-messages').append($(`<li>${message}</li>`));
-  $('#status-messages').show();
-}
-
-const addNewTask = function(event) {
-  event.preventDefault();
-
-  const taskData ={};
-  ['task_name', 'assignee'].forEach( (field) => {
-    const val = $(`input[name=${field}]`).val();
-    if (val != '') {
-      taskData[field] = val;
-    }
-  });
-  const newTask = new Task(taskData);
-
-  if (newTask.isValid()) {
-    taskList.add(newTask);
-    updateStatusMessageWith(`New task added: ${newTask.get('task_name')}`);
-  } else {
-    updateStatusMessageFrom(newTask.validationError);
-  }
-}
-
 $(document).ready( () => {
-  $('#add-task-form').submit(addNewTask);
-
-  taskList.on('update', renderList, taskList);
+  const taskListView = new TaskListView({
+    model: taskList,
+    template: _.template($('#task-template').html()),
+    el: 'main'
+  });
 
   taskList.add(new Task({task_name: "Put rendering logic in Backbone Views", assignee: "Me"}));
   taskList.add(new Task({task_name: "Put handling events in Backbone Views", assignee: "Me"}));
---
+++ src/views/task_list_view.js
@@ -0,0 +1,74 @@
+import Backbone from 'backbone';
+import _ from 'underscore';
+import TaskView from '../views/task_view';
+import Task from '../models/task';
+
+const TaskListView = Backbone.View.extend({
+  initialize(params) {
+    this.template = params.template;
+    this.listenTo(this.model, 'update', this.render);
+  },
+  render() {
+    this.$('#todo-items').empty();
+    this.model.each((task) => {
+      const taskView = new TaskView({
+        model: task,
+        template: this.template,
+        tagName: 'li',
+        className: 'task',
+      });
+      this.$('#todo-items').append(taskView.render().$el);
+    });
+    return this;
+  },
+  events: {
+    'click #add-new-task': 'addTask'
+  },
+  addTask: function(event) {
+    event.preventDefault();
+    const taskData ={};
+    ['task_name', 'assignee'].forEach( (field) => {
+      const val = this.$(`input[name=${field}]`).val();
+      if (val != '') {
+        taskData[field] = val;
+      }
+    });
+    const newTask = new Task(taskData);
+
+    if (newTask.isValid()) {
+      this.model.add(newTask);
+      this.updateStatusMessageWith(`New task added: ${newTask.get('task_name')}`);
+    } else {
+      this.updateStatusMessageFrom(newTask.validationError);
+    }
+  },
+  updateStatusMessageFrom: function(messageHash) {
+    const statusMessagesEl = this.$('#status-messages');
+    statusMessagesEl.empty();
+    _.each(messageHash, (messageType) => {
+      messageType.forEach((message) => {
+        statusMessagesEl.append(`<li>${message}</li>`);
+      })
+    });
+    statusMessagesEl.show();
+  },
+  updateStatusMessageWith: function(message) {
+    const statusMessagesEl = this.$('#status-messages');
+    statusMessagesEl.empty();
+    statusMessagesEl.append(`<li>${message}</li>`);
+    statusMessagesEl.show();
+  }
+
+});
+
+
+
+
+
+
+
+
+
+
+
+export default TaskListView;
